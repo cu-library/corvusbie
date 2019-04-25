bb1f4f80bf286ef7dff274a9e0400b519fa90b3d
Kevin Bowrin <kevin.bowrin@carleton.ca>
2019-04-25 10:45:33 -0400

    Clean up js, add chat fallback

--- scripts/corvusbie.js
+++ scripts/corvusbie.js
@@ -1,16 +1,20 @@
 jQuery(document).ready(function($) {
+
+  // --- Move elements around the page ---
+
+  // Move the admin menu to the top of the page.
   $("#admin-menu").detach().prependTo('#page');
 
+  // Move the superfish menu to the menubar.
   $("#superfish-1-accordion").detach().appendTo('#menubar-wrapper');
 
+  // Move the chevron to the front of the last link in the desktop dropdown menus.
   $("#menu-bar li.sf-depth-2.last img").each(function(){
     var parent = $(this).parent();
     $(this).detach().prependTo(parent);
   });
-});
 
-//Subject Guide Accordion
-jQuery(document).ready(function($) {
+  // --- Subject guide accordions ---
 
   // Add permalinks to each detailed guide accordion content div.
   $('div.detailed-guide-accordion-content').each(function() {
@@ -34,27 +38,7 @@ jQuery(document).ready(function($) {
     $(hash).filter("a.guide-accordion-header-wrapper").click();
   }
 
-});
-
-//Course Reserves search
-function submit_form() {
-  var selind = document.forms.course_reserves_search.search_action.selectedIndex;
-  var selval = document.forms.course_reserves_search.search_action[selind].value;
-  document.forms.course_reserves_search.action=selval;
-  document.forms.course_reserves_search.submit();
-}
-
-//Journal Articles in Scholars Portal search
-function search_scholars_portal_journals() {
-var url="http://proxy.library.carleton.ca/login?url=http://journals2.scholarsportal.info./search-advanced.xqy?q=";
-var input_element = document.getElementById("search_terms");
-var end_of_url = "&search_in=anywhere&date_from=&date_to=&sort=relevance&sub=";
-document.location = url + encodeURIComponent(input_element.value) + end_of_url;
-}
-
-//Subject Guides - Limit Number of Accordions
-jQuery(document).ready(function($) {
-
+  // --- Limit number of accordions on edit screen for subject guides.
   function checkAndDisable(){
     var index = $('#edit-field-detailed-guide-section input[name="field_detailed_guide_section_add_more"]').parent().prev().find('tr:last').index();
     if (index >= 4) {
@@ -73,7 +57,62 @@ jQuery(document).ready(function($) {
     myObserver.observe(target, config);
   }
 
+  // --- Help and Chat Widget (Floaty Thing) Activation ---
+  function activateChat() {
+    $("#helpandchatimg").attr("alt", "discussion icon");
+    $("#helpandchatimg").attr("src", "/sites/all/themes/corvusbie/images/comment-discussion.svg");
+    $("#helpandchattext").text("Ask a Librarian");
+    $("#helpandchatlink").attr("href", "#");
+    $("#helpandchatlink").attr("onclick", "window.open(\"https://ca.libraryh3lp.com/chat/carleton@chat.ca.libraryh3lp.com?skin=15653\", \"AskUs\", \"resizable=1,width=375,height=350\"); return false;");
+  }
+
+  function deactivateChat() {
+    $("#helpandchatimg").attr("alt", "question icon");
+    $("#helpandchatimg").attr("src", "/sites/all/themes/corvusbie/images/question.svg");
+    $("#helpandchattext").text("Help");
+    $("#helpandchatlink").attr("href", "/help");
+    $("#helpandchatlink").attr("onclick", null);
+  }
+
+  function checkhelpavail() {
+    // We can't directly check for errors from getScript (jquery 1.4.4, and cross-origin script request).
+    // So, we add this hack: if we don't clear the timeout in 2 seconds, deactivate chat.
+    var helpError;
+    helpError = window.setTimeout(function() {
+        deactivateChat();
+    }, 2000);
+    $.getScript("https://ca.libraryh3lp.com/presence/jid/carletonn/chat.ca.libraryh3lp.com/js", function(data, textStatus, jqxhr) {
+      if ((Array.isArray(jabber_resources)) && (jabber_resources.length > 0)) {
+        if (jabber_resources[0]["show"] == "available") {
+          window.clearTimeout(helpError);
+          activateChat();
+        }
+      }
+    });
+  }
+
+  checkhelpavail();
+  window.setInterval(function(){
+     checkhelpavail();
+  }, 15000);
 });
+
+//Course Reserves search
+function submit_form() {
+  var selind = document.forms.course_reserves_search.search_action.selectedIndex;
+  var selval = document.forms.course_reserves_search.search_action[selind].value;
+  document.forms.course_reserves_search.action=selval;
+  document.forms.course_reserves_search.submit();
+}
+
+//Journal Articles in Scholars Portal search
+function search_scholars_portal_journals() {
+   var url="http://proxy.library.carleton.ca/login?url=http://journals2.scholarsportal.info./search-advanced.xqy?q=";
+   var input_element = document.getElementById("search_terms");
+   var end_of_url = "&search_in=anywhere&date_from=&date_to=&sort=relevance&sub=";
+   document.location = url + encodeURIComponent(input_element.value) + end_of_url;
+}
+
 
 // Close the sidr menu on desktop.
 (function ($) {
@@ -85,7 +124,7 @@ jQuery(document).ready(function($) {
         return;
       }
 
-      var activeTheme = Drupal.settings["ajaxPageState"]["theme"];
+      var activeTheme = Drupal.settings['ajaxPageState']['theme'];
       var themeSettings = Drupal.settings['adaptivetheme'];
 
       if ((typeof themeSettings == 'undefined') || (typeof themeSettings[activeTheme] == 'undefined')) {
@@ -105,5 +144,3 @@ jQuery(document).ready(function($) {
     }
   };
 })(jQuery);
-
-
